cef52912cbc4536c232dcbd61d406845661cecd3
Poonam M Shettar <97241199+poonamshettar@users.noreply.github.com>
2024-11-05 10:41:34 +0530

    Time: 4 ms (30.85%), Space: 12.1 MB (87.26%) - LeetHub

---
+++ 2914-minimum-number-of-changes-to-make-binary-string-beautiful/2914-minimum-number-of-changes-to-make-binary-string-beautiful.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int minChanges(string s) {
+        int c=0;
+        for(int i=0;i<s.length();i+=2)
+        {
+            if(s[i]!=s[i+1])
+            c+=1;
+        }
+        return c;
+    }
+};
